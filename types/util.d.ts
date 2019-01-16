a67a57beb3e367418422c5eca6c2c65f30899b8c
邓铃波 <deng.lingbo@mydreamplus.com>
2019-01-16 14:55:35 +0800

    0.5.3

--- types/util.d.ts
+++ types/util.d.ts
@@ -1,6 +1,10 @@
 export declare type ProtoType = '__actions__' | '__mutations__' | '__getters__' | 'state';
-export declare function getMethods(Store: ObjectConstructor, typeName: ProtoType): any;
-export declare function createMapping(context: any, key: ProtoType, funcName: string): void;
+interface StoreObjects {
+    [props: string]: any;
+}
+export declare function isContains(arr: Array<string | number>, matchArr: Array<string | number>): boolean;
+export declare function getMethods(Store: any, typeName: ProtoType): StoreObjects;
+export declare function createMapping(context: any, key: ProtoType, funcName: string): any;
 interface StateObject {
     [prop: string]: any;
 }
